170cd7d20b23a39c131ad3d38d725bb847517eae
Abhijeet Chkravorty <chakravorty.abhijeet91@gmail.com>
2021-12-30 13:30:23 +0530

    updated code for user and navigation component

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DataService } from '../services/data.service';
+import { Router } from '@angular/router';
 @Component({
-  selector: 'app-navigation',
-  templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+        selector: 'app-navigation',
+        templateUrl: './navigation.component.html',
+        styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+        constructor(public router: Router, public dataS: DataService) { }
 
-  ngOnInit(): void {
-  }
+        ngOnInit(): void {}
 
+        logout() {
+                this.dataS.removeLocal();
+                this.router.navigate(['/landing']);
+        }
 }
